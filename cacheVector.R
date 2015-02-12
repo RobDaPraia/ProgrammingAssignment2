3747b626e37a65b252e463a8c11e5082fd0f752e
Rob Da Praia <RobDaPraia@users.noreply.github.com>
2015-02-12 18:15:40 +0100

    First raw solutoin of assignment

--- cacheVector.R
+++ cacheVector.R
@@ -1,11 +1,56 @@
 ## cacheVector.R
 
+## Some help /explanations here: https://class.coursera.org/rprog-011/forum/thread?thread_id=105
 
-## The first function, makeVector creates a special "vector", which is really a list containing a function to
+
+
+## Let's say you have created a matrix and you have called cachemean() and the value is now cached.  
+## What if I want to change the values in the matrix?  If there isn't something to reset the values 
+## and invalidate the cache, the next call to cachemean() would return the wrong results.  
+## So $set() should, invalidate the cache and set the internals of the vector to the new values.
+
+## A read of ?"<<-" will help. The operators <<- and ->> are normally only used in functions, 
+## and cause a search to made through parent environments for an existing definition of the variable being assigned. 
+## If such a variable is found (and its binding is not locked) then its value is redefined, otherwise assignment 
+## takes place in the global environment. Note that their semantics differ from that in the S language, 
+## but are useful in conjunction with the scoping rules of R. See ‘The R Language Definition’ manual for further details and examples
+
+## Lexicon, Closures, good reads: 
+## http://stackoverflow.com/questions/5785290/what-is-the-difference-between-assign-and-in-r
+## http://adv-r.had.co.nz/Functional-programming.html#closures
+
+## http://stackoverflow.com/questions/24904683/caching-the-mean-of-a-vector-in-r
+##
+## m <- NULL begins by setting the mean to NULL as a placeholder for a future value
+## set <- function(y) {x <<- y; m <<- NULL} defines a function to set the vector, x, to a new vector, y, and resets the mean, m, to NULL
+## get <- function() x returns the vector, x
+## setmean <- function(mean) m <<- mean sets the mean, m, to mean
+## getmean <- function() m returns the mean, m
+## list(set = set, get = get,setmean = setmean,getmean = getmean) returns the 'special vector' containing all of the functions just defined
+
+## makeVector creates a special "vector", which is really a list containing a function to
 ## - set the value of the vector
 ## - get the value of the vector
 ## - set the value of the mean
 ## - get the value of the mean
+
+## Example run:
+## > x <- makeVector(c(1,2,3))
+## > x$get()
+## [1] 1 2 3
+## > cachemean(x)
+## [1] 2
+## > cachemean(x)
+## getting cached data
+## [1] 2
+## > x$set(c(4,5,6))
+## > x$get()
+## [1] 4 5 6
+## > cachemean(x)
+## [1] 5
+## > cachemean(x)
+## getting cached data
+## [1] 5
 
 makeVector <- function(x = numeric()) {
   m <- NULL
@@ -39,3 +84,4 @@ cachemean <- function(x, ...) {
   x$setmean(m)
   m
 }
+
--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,68 @@
 ## cachematrix.R
 
+## Note: In Excel you can inverse a matrix by "=MINVERSE(B2:C3)"
+## See: 
+## http://blog.modelworks.ch/?p=398
+## http://www.excelfunctions.net/Excel-Minverse-Function.html
+
+## Matrix calculations in R: 
+## http://www.ats.ucla.edu/stat/r/library/matrix_alg.htm
+## http://www.r-tutor.com/r-introduction/matrix/matrix-construction
+
+
 ## Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
 
+
+## Example run:
+## > my_matrix = matrix( c(1, 1, 3, 1), nrow=2, ncol=2)
+## > x <- makeCacheMatrix(my_matrix)
+## > x$get()
+## [,1] [,2]
+## [1,]    1    3
+## [2,]    1    1
+## > cacheSolve(x)
+## [,1] [,2]
+## [1,] -0.5  1.5
+## [2,]  0.5 -0.5
+## > cacheSolve(x)
+## getting inversed matrix
+## [,1] [,2]
+## [1,] -0.5  1.5
+## [2,]  0.5 -0.5
+
 makeCacheMatrix <- function(x = matrix()) {
+  
+  m2 <- NULL
+  set <- function(y) {
+    x <<- y
+    m2 <<- NULL
+  }
+  get <- function() x
+  setSolved <- function(solved) m2 <<- solved
+  getSolved<- function() m2
+  list(set = set, get = get,
+       setSolved = setSolved,
+       getSolved = getSolved)
 
 }
 
 
 ## Write a short comment describing this function
 
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  m2 <- x$getSolved()
+  if(!is.null(m2)) {
+    message("getting inversed matrix")
+    return(m2)
+  }
+  data <- x$get()
+  m2 <-  solve(data, ...)
+  x$setSolved(m2)
+  m2
 }
+
